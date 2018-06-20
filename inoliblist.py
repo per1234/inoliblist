9c88c2b68ae24d7fbabf0778b73945150d1668fd
per1234 <accounts@perglass.com>
2018-06-19 18:36:49 -0700

    Log message when stored requests remaining value doesn't match actual value

--- inoliblist.py
+++ inoliblist.py
@@ -363,6 +363,8 @@ def check_rate_limiting(api_type):
                     notification_timestamp = time.time()
             # leave the last_api_requests_remaining_value[api_type] set to 0
             # this will cause the actual value to be pulled from the API on the next check_rate_limiting() call
+        else:
+            logger.info("Mismatch between stored requests remaining value (0) and actual value")
 
 
 def get_json_from_url(url):
